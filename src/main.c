e33b11433750cd8b7f6d877b3c9d748f3a767f41
Tom Atkinson <me@tomatkinson.uk>
2021-05-24 16:54:27 +0100

    Add very simple echo server
    This is a trivial echo server that sends the first 1024 bytes to the
    sender. This first pass is a single-threaded, blocking server that
    can't respond correctly to any HTTP requests. It handles all errors,
    though not necessarily in the best way, and there is no logging
    functionality, syslog or otherwise

--- src/main.c
+++ src/main.c
@@ -3,8 +3,62 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+#define BACKLOG 10
+#define BUFSIZ 1024
 
 int main() {
-    printf("Hello, World!\n");
+    printf("http_server v0.0.1\n");
+    
+    /* Create a new IPv4 TCP socket to listen on */
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    struct sockaddr_in serv_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(PORT),
+        .sin_addr.s_addr = INADDR_ANY
+    };
+    
+    int err = bind(sockfd, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
+    if (err == -1) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+    
+    err = listen(sockfd, BACKLOG);
+    if (err == -1) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        struct sockaddr_in peeraddr;
+        socklen_t peerlen;
+
+        int connfd = accept(sockfd, (struct sockaddr *) &peeraddr, &peerlen);
+        if (connfd == -1) {
+            perror("accept");
+        }
+
+        char buf[BUFSIZ] = {'\0'};
+        ssize_t recvd = recv(connfd, buf,BUFSIZ, 0);
+        if (recvd == -1) {
+            perror("recv");
+        }
+
+        printf("buf: %s\n", buf);
+        ssize_t sent = send(connfd, buf, BUFSIZ, 0);
+        if (sent == -1) {
+            perror("send");
+        }
+
+        close(connfd);
+    }
     return 0;
 }
